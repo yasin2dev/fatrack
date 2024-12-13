6b98cd62c8a6d7fbcf1fd355615254401e2e1b2f
yasin2dev <yasin.bozkurt.n@gmail.com>
2024-12-14 00:06:45 +0300

    add server GET method to fatura and minor model update on fatura

--- src/lib/modals/fatura.ts
+++ src/lib/modals/fatura.ts
@@ -4,10 +4,11 @@ import {Schema, model, models} from "mongoose";
 const FaturaSchema = new Schema(
     {
         title: {type: "string", required: true},
-        origin: {type: Number, require: true},
-        root_amount: {type: Number, require: false},
-        tax_val: {type: Number, require: false},
-        total: {type: Number, require: true},
+        origin: {type: "number", require: true},
+        root_amount: {type: "number", require: false},
+        tax_val: {type: "number", require: false},
+        total: {type: "number", require: true},
+        owner: {type: Schema.Types.ObjectId, ref: "User"}
     },
     {
         timestamps: true
