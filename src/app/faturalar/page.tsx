8f84597b01c26f2a791502f383a4711ae7cd6a1d
yasin2dev <yasin.bozkurt.n@gmail.com>
2024-12-18 22:16:46 +0300

    adding announcements to Home. Not done yet.

--- src/app/faturalar/page.tsx
+++ src/app/faturalar/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from 'react';
 import axios from "axios"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons'; 
 
 interface Data {
     _id: string;
@@ -29,13 +31,14 @@ function Page() {
     const [error, setError] = useState<string | null>(null);
     useEffect(() => {
         // API'ye istek atıyoruz
-        axios.get<Data[]>('http://localhost:3000/api/faturalar', {
+        axios.get<Data[]>(`${process.env.NEXT_PUBLIC_BASE_URL}/api/faturalar`, {
             params: { "userId": "675b5ff200e40d7c11cf81b6" }, // Example user ID from mongodb.
             headers: {
                 "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`, // For now, using manuel token system. Auth system inProgress
             }
         })
             .then(response => {
+                /* TODO: VSCode showing error in here. fix. */
                 setData(response.data.faturalar);
                 setLoading(false);
             })
@@ -53,18 +56,23 @@ function Page() {
         return <div className={"text-center my-2"}>Hata: {error}</div>;
     }
 
+    /* TODO: VSCode showing error in here. fix. */
     if (Object.keys(data).length <= 0) {
         return <div>NO DATA</div>
     }
 
-    {/* TODO: Data successfully fetched and listed in front-end Fatura's now can be list dynamically */}
+    {/* TODO: Data successfully fetched and listed in front-end Fatura's now can be list dynamically */ }
     return (
         <div className={"justify-center mx-10"}>
             {process.env.TOKEN}
             {data?.map(fatura => (
-                <div className='text-center bg-amber-500 my-5 mx-2 rounded p-2 w-full h-16 justify-center pl-6'>
-                    <p><span className={"font-bold"}>Ürün: </span>{fatura.title} - <span className={"font-bold text-black"}>Fatura No: </span>{fatura.fatura_no}</p>
-                    <p><span className={"text-center"}>Tutar: {fatura.total}{fatura.birim}</span></p>
+                <div className='bg-blue-700 my-5 mx-2 rounded p-2 w-full h-fit pl-6'>
+                    <div className='flex'>
+                        <p><span className={"font-bold text-right"}>Ürün: </span>{fatura.title} - <span className={"font-bold"}>Fatura No: </span>{fatura.fatura_no}</p>
+                    </div>
+                    <FontAwesomeIcon icon={faTrash} className={"fa fa-trash float-end cursor-pointer hover:text-red-500 mr-6"}/>
+                    <p><span className={"font-bold"}>{fatura.origin}</span></p>
+                    <p><span className={"items-end"}>Tutar: {fatura.total} {fatura.birim}</span></p>
                 </div>
             ))}
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import PagePlaceholder from "@/components/page-placeholder";
+"use client"
+
+import { useEffect, useState} from "react";
+
+interface AnnData {
+  id: string;
+  title: string;
+  content: string;
+  publishDate: Date;
+  category: string;
+  status: 'active' | 'inactive' | 'deleted';
+}
+
 
 export default function Home() {
+  const [data, setData] = useState<AnnData[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
   return (
-      <div className={"flex flex-row mx-auto my-2 w-full justify-between"}>
-        <PagePlaceholder pageName={"Home"}/>
+      <div className={"my-2 flex mx-auto"}>
+        <div className="bg-blue-700 text-center mx-6 rounded-md h-max pb-4 pt-4">
+          
+          <p></p>
+        </div>
       </div>
   );
 }
