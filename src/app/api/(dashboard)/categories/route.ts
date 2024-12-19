a6d8d3d26cdab0d4bc6f5198a19415d4c7e6a85f
yasin2dev <yasin.bozkurt.n@gmail.com>
2024-12-19 23:04:07 +0300

    delete ignored commented lines

--- src/app/api/(dashboard)/categories/route.ts
+++ src/app/api/(dashboard)/categories/route.ts
@@ -6,30 +6,6 @@ import { Types } from "mongoose";
 
 export const GET = async (request: Request) => {
   try {
-    // const { searchParams } = new URL(request.url);
-    // const userId = searchParams.get("userId");
-
-    // if (!userId || !Types.ObjectId.isValid(userId)) {
-    //   return new NextResponse(
-    //     JSON.stringify({ message: "Invalid or missing userId" }),
-    //     {
-    //       status: 400,
-    //     }
-    //   );
-    // }
-
-    // await connect();
-
-    // const user = await User.findById(userId);
-    // if (!user) {
-    //   return new NextResponse(
-    //     JSON.stringify({ message: "User not found in the database" }),
-    //     {
-    //       status: 400,
-    //     }
-    //   );
-    // }
-
     const categories = await Category.find();
 
     return new NextResponse(JSON.stringify(categories), {
